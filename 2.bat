bc4d0a7512dabe8092e255fc2f38086fe2c119e3
Jesusking112 <112292221+Jesusking112@users.noreply.github.com>
2025-05-05 23:22:20 -0500

    Add files via upload

---
+++ 2.bat
@@ -0,0 +1,9 @@
+@echo off
+set version=2.13.0
+echo version %version%
+echo ******************* MENU **********************
+echo 1) Nueva herramienta brutal
+echo 2) Comprimir videos.mp4
+echo 3) Lo que quieras
+echo a
+pause
