e1c81653396fa1cec98ce4aaf9fbe8284935a255
Jesusking112 <112292221+Jesusking112@users.noreply.github.com>
2025-05-06 00:55:22 -0500

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,8 +1,8 @@
-set version=2.24.0
+set version=2.25.0
 echo version %version%
 echo ******************* MENU **********************
 echo 1) dddc
-echo 2)
+echo 2) sEGUNDO INTWENTO
 echo 3)
 echo a
 echo e
