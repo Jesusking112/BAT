b4debd89b0d43de869ce3b1d236a8dcfbfaef752
Jesusking112 <112292221+Jesusking112@users.noreply.github.com>
2025-05-06 00:19:29 -0500

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,10 +1,10 @@
 @echo off
-set version=2.17.0
+set version=2.18.0
 echo version %version%
 echo ******************* MENU **********************
 echo 1) Nueva herramienta brutal
 echo 2) Comprimir videos.mp4
-echo 3) Lo que quieras
+echo 3)
 echo a
 echo e
 echo e
