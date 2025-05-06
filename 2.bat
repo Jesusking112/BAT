a232ce81158fa57bd5ea7f03aad8c72f4b555eb3
Jesusking112 <112292221+Jesusking112@users.noreply.github.com>
2025-05-06 00:38:11 -0500

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,8 +1,8 @@
 @echo off
-set version=2.21.0
+set version=2.22.0
 echo version %version%
 echo ******************* MENU **********************
-echo 1) Nueva herramienta brutal
+echo 1) Nueva herramien
 echo 2)
 echo 3)
 echo a
