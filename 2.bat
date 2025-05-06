7dc22be6f0813d455778341f0e1b49ef954ce031
Jesusking112 <112292221+Jesusking112@users.noreply.github.com>
2025-05-06 00:29:28 -0500

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -3,7 +3,7 @@ set version=2.19.0
 echo version %version%
 echo ******************* MENU **********************
 echo 1) Nueva herramienta brutal
-echo 2) Comprimir
+echo 2) Compri
 echo 3)
 echo a
 echo e
